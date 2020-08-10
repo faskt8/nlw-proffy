1b9fa597596d6286850c7cf929dbde2c6391f674
faskt8 <fabriciomfernandes@gmail.com>
2020-08-10 20:02:19 -0300

    Day Two , back end application

---
+++ server/src/routes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import ClassesControllers from './controllers/ClassesControllers';
+import ConnectionsControllers from './controllers/ConnectionsController';
+
+const routes = express.Router();
+const classesControllers = new ClassesControllers();
+const connectionsControllers = new ConnectionsControllers();
+
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
+
+routes.get('/connections', connectionsControllers.index);
+routes.post('/connections', connectionsControllers.create);
+
+export default routes;
